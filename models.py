db55eeab1b5c4ba0e698e90c3792f52092974d2c
Maksim Ilmast <maksim.ilmast@yandex.com>
2023-03-18 09:47:24 +0200

    preliminary search

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
+from sqlalchemy import func, or_, and_
 from db import db
+
 
 class Rejections(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,7 +12,10 @@ class Rejections(db.Model):
     reason_2 = db.Column(db.Text, nullable=True)
     reason_3 = db.Column(db.Text, nullable=True)
 
-def save_rejection(model_year, make, model, rejection_percentage, reason_1, reason_2, reason_3):
+
+def save_rejection(
+    model_year, make, model, rejection_percentage, reason_1, reason_2, reason_3
+):
     rejection = Rejections(
         model_year=model_year,
         make=make,
@@ -18,11 +23,28 @@ def save_rejection(model_year, make, model, rejection_percentage, reason_1, reas
         rejection_percentage=rejection_percentage,
         reason_1=reason_1,
         reason_2=reason_2,
-        reason_3=reason_3
+        reason_3=reason_3,
     )
     db.session.add(rejection)
     db.session.commit()
     return True
 
+
 def get_rejections():
     return db.session.execute(db.select(Rejections)).scalars().fetchall()
+
+
+def filter_rejections(searchbar_val):
+    searchbar_splitted = searchbar_val.split(" ")
+    if (searchbar_splitted[-1] == ''):
+        searchbar_val = searchbar_val.strip()
+    return Rejections.query.filter(
+        or_(
+            func.lower(Rejections.make).ilike(f"%{searchbar_val}%"),
+            func.lower(Rejections.model).ilike(f"%{searchbar_val}%"),
+            and_(
+                func.lower(Rejections.make).in_(searchbar_splitted),
+                func.lower(Rejections.model).in_(searchbar_splitted),
+            )
+    )).all() 
+
--- routes.py
+++ routes.py
@@ -1,24 +1,35 @@
 import json
 import models
 from app import app
-from flask import render_template, redirect, request, abort, session
+from flask import render_template, redirect, request
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     rejections = models.get_rejections()
     return render_template("index.html", rejections=rejections)
 
+
+@app.route("/filter")
+def filter():
+    searchbar_val = request.args.get("searchbar_val")
+    filtered_rejections = models.filter_rejections(searchbar_val)
+    return render_template("filter.html", rejections=filtered_rejections)
+
+
 @app.route("/vehicle-data", methods=["POST"])
 def vehicle_data():
-    vehicle_data_input_file = request.files['vehicle-data-input']
+    vehicle_data_input_file = request.files["vehicle-data-input"]
     vehicle_data = json.load(vehicle_data_input_file)
     for data_obj in vehicle_data:
         model_year = int(data_obj["model_year"])
         make = data_obj["make"]
         model = data_obj["model"]
-        rejection_percentage = float(data_obj["rejection_percentage"].replace(',', '.'))
+        rejection_percentage = float(data_obj["rejection_percentage"].replace(",", "."))
         reason_1 = data_obj["reason_1"]
         reason_2 = data_obj["reason_2"]
         reason_3 = data_obj["reason_3"]
-        models.save_rejection(model_year, make, model, rejection_percentage, reason_1, reason_2, reason_3)
+        models.save_rejection(
+            model_year, make, model, rejection_percentage, reason_1, reason_2, reason_3
+        )
     return redirect("/")
