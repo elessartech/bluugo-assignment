826f4947e6eddfc4947845e564eaa2f43909f1a6
Maksim Ilmast <maksim.ilmast@yandex.com>
2023-03-19 17:42:10 +0200

    ts-vector intoduced

--- models.py
+++ models.py
@@ -1,7 +1,15 @@
 from db import db
+from sqlalchemy import Index, func, cast
+from sqlalchemy.dialects import postgresql
 
+def create_tsvector(*args):
+    colummns = args[0]
+    for column in args[1:]:
+        colummns += ' ' + column
+    return func.to_tsvector('english', colummns)
 
 class Vehicle(db.Model):
+    __tablename__ = 'vehicle'
     id = db.Column(db.Integer, primary_key=True)
     model_year = db.Column(db.Integer, nullable=False)
     make = db.Column(db.Text, nullable=False)
@@ -10,3 +18,13 @@ class Vehicle(db.Model):
     reason_1 = db.Column(db.Text, nullable=True)
     reason_2 = db.Column(db.Text, nullable=True)
     reason_3 = db.Column(db.Text, nullable=True)
+    __ts_vector__ = create_tsvector(
+        cast(func.to_tsvector('english', make + " " + model + " " + reason_1 + " " + reason_2 + " " + reason_3), postgresql.TEXT)
+    )
+    __table_args__ = (
+        Index(
+            'ix_vehicle_fts',
+            func.to_tsvector('english', make + " " + model + " " + reason_1 + " " + reason_2 + " " + reason_3),
+            postgresql_using='gin'
+        ),
+    )
--- queries.py
+++ queries.py
@@ -46,20 +46,21 @@ def update_vehicle(id, rejection_percentage, reason_1, reason_2, reason_3):
 
 
 def filter_vehicles(searchbar_val):
-    searchbar_splitted = searchbar_val.lower().split(" ")
-    if searchbar_splitted[-1] == "":
-        searchbar_val = searchbar_val.strip()
-    return (
-        Vehicle.query.filter(
-            or_(
-                func.lower(Vehicle.make).ilike(f"%{searchbar_val}%"),
-                func.lower(Vehicle.model).ilike(f"%{searchbar_val}%"),
-                and_(
-                    func.lower(Vehicle.make).in_(searchbar_splitted),
-                    func.lower(Vehicle.model).in_(searchbar_splitted),
-                ),
-            )
-        )
-        .limit(50)
-        .all()
-    )
+    return Vehicle.query.filter(Vehicle.__ts_vector__.match(searchbar_val)).all()
+    #searchbar_splitted = searchbar_val.lower().split(" ")
+    #if searchbar_splitted[-1] == "":
+    #    searchbar_val = searchbar_val.strip()
+    #return (
+    #    Vehicle.query.filter(
+    #        or_(
+    #            func.lower(Vehicle.make).ilike(f"%{searchbar_val}%"),
+    #            func.lower(Vehicle.model).ilike(f"%{searchbar_val}%"),
+    #            and_(
+    #                func.lower(Vehicle.make).in_(searchbar_splitted),
+    #                func.lower(Vehicle.model).in_(searchbar_splitted),
+    #            ),
+    #        )
+    #    )
+    #    .limit(50)
+    #    .all()
+    #)
