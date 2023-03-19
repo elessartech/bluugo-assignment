583ca28a957c85918256119cbf69729e9fdc45de
Maksim Ilmast <maksim.ilmast@yandex.com>
2023-03-19 08:49:58 +0200

    csrf introduced

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+csrf = CSRFProtect()
 
 app = Flask(__name__)
 
 app.config.from_object("config")
+
+csrf.init_app(app)
 
 import routes
